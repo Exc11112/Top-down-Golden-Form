61a6e5f35d3e78f430d092bb3ba2d8d850c25cf2
641210459 Ice <ice54321.ih@gmail.com>
2024-02-11 04:45:44 +0700

    add animation for player

--- Top down EIEI/Assets/Player/PlayerContoller.cs
+++ Top down EIEI/Assets/Player/PlayerContoller.cs
@@ -1,20 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerContoller : MonoBehaviour
 {
+    public Animator anim;
     public float speed;
-    private Vector2 move;
-    // Start is called before the first frame update
+    public Vector2 move;
+
+    private bool awalk;
+
     void Start()
     {
-        
+        // Initialization code here if needed
     }
 
-    // Update is called once per frame
     void Update()
     {
         movePlayer();
@@ -24,9 +27,9 @@ public class PlayerContoller : MonoBehaviour
     {
         Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        if (movement != Vector3.zero) //อันนี้คือที่ทำให้ทรงกระบอกหมุนได้
+        if (movement != Vector3.zero)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f); //อันนี้คือที่ทำให้ทรงกระบอกหมุนได้
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
         }
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
@@ -35,5 +38,22 @@ public class PlayerContoller : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
+        awalk = move.magnitude > 0;
+
+        anim.SetBool("walkis", awalk);
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            speed *= 2f; // Adjust the running speed as needed
+            anim.SetBool("runis", true);
+        }
+        else if (context.canceled)
+        {
+            speed /= 2f; // Reset speed when Shift key is released
+            anim.SetBool("runis", false);
+        }
     }
 }
