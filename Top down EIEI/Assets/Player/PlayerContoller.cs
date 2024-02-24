c4b0775cd317d24a2e987ac8760351c99f65fc26
icezazazz120 <ice54321.ih@gmail.com>
2024-02-25 04:08:37 +0700

    Change move code

--- Top down EIEI/Assets/Player/PlayerContoller.cs
+++ Top down EIEI/Assets/Player/PlayerContoller.cs
@@ -5,20 +5,26 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerContoller : MonoBehaviour
 {
     public Animator anim;
-    public float speed;
+    public float walkSpeed = 8.0f;
+    public float runMultiplier = 1.5f;
+    private float currentSpeed;
     public Vector2 move;
     private Vector3 initialPosition;
-
+    private bool isRunning = false;
+    //public Image Box, Money, Jewelry, Skull, Gold;
 
     private bool awalk;
 
     void Start()
     {
         initialPosition = transform.position;
+        //Box.gameObject.SetActive(false);
+        currentSpeed = walkSpeed;
     }
 
     void Update()
@@ -28,39 +34,76 @@ public class PlayerContoller : MonoBehaviour
 
     public void movePlayer()
     {
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
 
         if (movement != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
         }
 
-        //Vector3 smoothedMovement = Vector3.Lerp(transform.position, transform.position + movement, 0.1f);
-        //transform.position = smoothedMovement;
+        transform.Translate(movement * currentSpeed * Time.deltaTime, Space.World);
 
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        move = context.ReadValue<Vector2>();
-        awalk = move.magnitude > 0;
-
+        awalk = movement.magnitude > 0;
         anim.SetBool("walkis", awalk);
+
+        checkRunInput();
     }
 
-    public void OnRun(InputAction.CallbackContext context)
+    //public void movePlayer()
+    //{
+    //    Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+    //    if (movement != Vector3.zero)
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+    //    }
+
+    //    //Vector3 smoothedMovement = Vector3.Lerp(transform.position, transform.position + movement, 0.1f);
+    //    //transform.position = smoothedMovement;
+
+    //    transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    //}
+
+    //public void OnMove(InputAction.CallbackContext context)
+    //{
+    //    move = context.ReadValue<Vector2>();
+    //    awalk = move.magnitude > 0;
+
+    //    anim.SetBool("walkis", awalk);
+    //}
+
+    //public void OnRun(InputAction.CallbackContext context)
+    //{
+    //    if (context.performed)
+    //    {
+    //        speed *= 1.5f; // Adjust the running speed as needed
+    //        anim.SetBool("runis", true);
+    //    }
+    //    else if (context.canceled)
+    //    {
+    //        speed /= 1.5f; // Reset speed when Shift key is released
+    //        anim.SetBool("runis", false);
+    //    }
+    //}
+
+    public void checkRunInput()
     {
-        if (context.performed)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed *= 1.5f; // Adjust the running speed as needed
+            isRunning = true;
             anim.SetBool("runis", true);
         }
-        else if (context.canceled)
+        else
         {
-            speed /= 1.5f; // Reset speed when Shift key is released
+            isRunning = false;
             anim.SetBool("runis", false);
         }
+
+        // Adjust speed based on whether the player is running or not
+        currentSpeed = isRunning ? walkSpeed * runMultiplier : walkSpeed;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -77,4 +120,5 @@ public class PlayerContoller : MonoBehaviour
         transform.position = initialPosition;
         SceneManager.LoadScene("1");
     }
+
 }
