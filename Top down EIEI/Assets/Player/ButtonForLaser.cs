fd1c8f4e76eb8256021016bb0d008f801a3671d0
icezazazz120 <ice54321.ih@gmail.com>
2024-03-13 01:51:58 +0700

    change camera view and update visual on button

--- Top down EIEI/Assets/Player/ButtonForLaser.cs
+++ Top down EIEI/Assets/Player/ButtonForLaser.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonForLaser : MonoBehaviour
 {
     [SerializeField] GameObject laser;
+    public SpriteRenderer spriteRenderer; // Reference to the SpriteRenderer component
+    public Sprite newSprite; // The new sprite to assig
+
+    void Start()
+    {
+        // Get references to the SpriteRenderer and Button components
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnTriggerStay(Collider collision)
     {
@@ -14,6 +23,7 @@ public class ButtonForLaser : MonoBehaviour
             if (Input.GetKey(KeyCode.E))
             {
                 laser.GetComponent<ButtonForLaser>().CloseLaser();
+                ChangeSprite();
             }
         }
     }
@@ -21,5 +31,14 @@ public class ButtonForLaser : MonoBehaviour
     public void CloseLaser()
     {
         gameObject.SetActive(false);
+    }
+
+    void ChangeSprite()
+    {
+        // Change the sprite of the SpriteRenderer to the new sprite
+        if (spriteRenderer != null && newSprite != null)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
     }
 }
